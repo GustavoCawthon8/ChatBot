e88adf4b735042e85a96f7c0033ca13dd184be7d
englishtemaedition <englishtemaedition@gmail.com>
2025-09-28 16:18:10 +0000

    trocando os tipos

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/perguntar", async(req, res)=>{
 })
 
 
-db.sync({force: true}).then(()=>{
+db.sync({force: false}).then(()=>{
     app.listen(PORT, ()=>{
         console.log(`Server running on port https://localhost:${PORT}`)
     })
